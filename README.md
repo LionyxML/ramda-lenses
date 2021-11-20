3f1daf88b63ba04750ee1c4e67e206018bfecfff
LionyxML <rahul.juliato@gmail.com>
2021-11-19 21:10:29 -0300

    All examples added + the expected output

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # ramda-lenses
 
 A simple example repo of how to use Lenses in JS with the Ramda library.
+
+You should install the dependencies with:
+`npm install`
+
+And then run the js file with:
+`node index.js`
+
+If you are not willing to install anything and you're learning by simply reading the code, you may find this program output inside the "output.txt" file, just for convenience.
--- index.js
+++ index.js
@@ -1,1 +1,78 @@
-import * as R from "ramda";
+const R = require("ramda");
+
+// Our data
+const user = {
+  id: 1,
+  name: "foo",
+  company: {
+    id: 12,
+    name: "bar",
+    address: {
+      street: "randomstreet",
+    },
+  },
+  comments: [
+    { id: 2, text: "yes, this could work.", to: { id: 4 } },
+    { id: 3, text: "not sure.", to: { id: 12 } },
+    { id: 4, text: "well, maybe", to: { id: 4 } },
+  ],
+};
+
+console.log(">>>> Example 1");
+// Setting a lens for the Id
+const idLens = R.lensProp("id");
+
+// Now it is possible to view the content of this prop with...
+console.log(R.view(idLens, user));
+
+// And to set a new value to this prop (actualy creates a copy, it does
+// not mutate the original)...
+console.log(R.set(idLens, 2, user));
+
+console.log("\n>>>> Example 2");
+
+// A new lens now inside with nested props
+const userCoStrLens = R.lensPath(["company", "address", "street"]);
+
+// Showing the value
+console.log(R.view(userCoStrLens, user));
+
+// Updating the value
+console.log(R.set(userCoStrLens, "Foobarstreet", user));
+
+console.log("\n>>>> Example 3");
+
+// A lens to nested props with an array
+const firstCommentLensId = R.lensPath(["comments", 0, "id"]);
+console.log(R.view(firstCommentLensId, user));
+
+// Updating the value
+console.log(R.set(firstCommentLensId, 12, user));
+
+console.log("\n>>>> Example 4");
+
+// If we want to apply a function we should use "over"
+const firstCommentTextLens = R.lensPath(["comments", 0, "text"]);
+console.log(R.over(firstCommentTextLens, R.toUpper, user));
+
+console.log("\n>>>> Example 5");
+
+// We can even use "compose"!
+const companyLens = R.lensProp("company");
+const addressLens = R.lensProp("address");
+const streetLens = R.lensProp("street");
+const addressStreetLens = R.compose(companyLens, addressLens, streetLens);
+console.log(R.view(addressStreetLens, user));
+
+console.log("\n>>>> Example 6");
+
+// Or compose like this
+const commentLens = R.lensProp("comments");
+const firstIndexLens = R.lensIndex(0);
+const idLens2 = R.lensProp("id");
+const composedLens = R.compose(commentLens, firstIndexLens, idLens2);
+console.log(R.view(composedLens, user));
+
+console.log("\n>>>> Example7");
+const textLens = R.lensProp("text");
+console.log(R.over(commentLens, R.map(R.over(textLens, R.toUpper)), user));
